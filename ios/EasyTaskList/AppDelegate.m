7906532fb41485fd366ee4df7f643efd73631925
Matheus Botelho <matheus.botelho97@gmail.com>
2020-09-07 18:02:28 -0300

    Ajuste ios react native 0.63.2

--- ios/EasyTaskList/AppDelegate.m
+++ ios/EasyTaskList/AppDelegate.m
@@ -6,7 +6,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
-#if DEBUG
+#ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
 #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
@@ -29,14 +29,14 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-#if DEBUG
+#ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
 
   if([FIRApp defaultApp] == nil){
     [FIRApp configure];
   }
-  
+
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"EasyTaskList"
