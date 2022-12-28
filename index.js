184620f9a980314f95a65fae59adbd98fd97686f
Matheus Botelho <matheusbcdev@gmail.com>
2022-12-28 18:09:59 -0300

    update rn 0.70.6

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 /**
  * @format
  */
-import 'react-native-gesture-handler';
+
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
-
-if (__DEV__) {
-  import('./src/config/ReactotronConfig').then(() =>
-    console.log('Reactotron Configured'),
-  );
-}
 
 AppRegistry.registerComponent(appName, () => App);
