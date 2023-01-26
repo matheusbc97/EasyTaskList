634a80a5d7c6c5b571f5a4aa22bd68899df8baa4
Matheus Botelho <matheusbcdev@gmail.com>
2023-01-26 15:36:58 -0300

    EmptyListText tests

---
+++ __tests__/shared/components/EmptyListText.test.tsx
@@ -0,0 +1,21 @@
+import {EmptyListText} from '@/shared/components';
+import {render} from '@testing-library/react-native';
+import {TEST_IDS} from '@/shared/constants/testIds';
+
+jest.mock('@/shared/hooks/data/useAppTheme');
+
+describe('EmptyListText Component', () => {
+  it('Should render an EmptyListText', () => {
+    render(<EmptyListText text="Testing" />);
+  });
+
+  it('Should have text "Testing"', async () => {
+    const text = 'Testing';
+
+    const {findByTestId} = render(<EmptyListText text={text} />);
+
+    const element = await findByTestId(TEST_IDS.TEXT_BASE);
+
+    expect(element).toHaveTextContent(text);
+  });
+});
