c213e0babaf32f4f3895ca44de22e0de10f05e4e
Matheus Botelho <matheusbcdev@gmail.com>
2023-01-27 17:03:29 -0300

    Update Header.test.tsx

--- __tests__/shared/components/Header.test.tsx
+++ __tests__/shared/components/Header.test.tsx
@@ -5,12 +5,12 @@ import {TEST_IDS} from '@/shared/constants/testIds';
 
 jest.mock('@/shared/hooks/data/useAppTheme');
 
-const mockedGoBack = jest.fn();
+const mockedNavigationGoBack = jest.fn();
 
 jest.mock('@react-navigation/native', () => {
   return {
     useNavigation: () => ({
-      goBack: mockedGoBack,
+      goBack: mockedNavigationGoBack,
     }),
   };
 });
@@ -45,7 +45,7 @@ describe('Header Component', () => {
 
     fireEvent.press(primaryHeaderBackButton);
 
-    expect(mockedGoBack).toHaveBeenCalledTimes(1);
+    expect(mockedNavigationGoBack).toHaveBeenCalledTimes(1);
   });
 
   it('Should fire SecondaryHeader navigation go back method', async () => {
@@ -57,7 +57,39 @@ describe('Header Component', () => {
 
     fireEvent.press(secondaryHeaderBackButton);
 
-    expect(mockedGoBack).toHaveBeenCalledTimes(2);
+    expect(mockedNavigationGoBack).toHaveBeenCalledTimes(2);
+  });
+
+  it('Should fire PrimaryHeader onBackPress', async () => {
+    const onBackPress = jest.fn();
+
+    const {findByTestId} = render(
+      <Header title="Testing" onBackPress={onBackPress} />,
+    );
+
+    const primaryHeaderBackButton = await findByTestId(
+      TEST_IDS.PRIMARY_HEADER_BACK_BUTTON,
+    );
+
+    fireEvent.press(primaryHeaderBackButton);
+
+    expect(onBackPress).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should fire SecondaryHeader onBackPress', async () => {
+    const onBackPress = jest.fn();
+
+    const {findByTestId} = render(
+      <Header title="Testing" type="secondary" onBackPress={onBackPress} />,
+    );
+
+    const secondaryHeaderBackButton = await findByTestId(
+      TEST_IDS.SECONDARY_HEADER_BACK_BUTTON,
+    );
+
+    fireEvent.press(secondaryHeaderBackButton);
+
+    expect(onBackPress).toHaveBeenCalledTimes(1);
   });
 
   it('Should show text "Testing" on Primary Header', async () => {
