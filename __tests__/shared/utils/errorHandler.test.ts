9e24f1d2861c112bdd6063ccba18d2dc72b6c669
Matheus Botelho <matheusbcdev@gmail.com>
2023-01-31 15:15:20 -0300

    errorHandler test

---
+++ __tests__/shared/utils/errorHandler.test.ts
@@ -0,0 +1,19 @@
+import {handleErrorMessage} from '@/shared/utils/errorHandler';
+
+let mockedShowToast: jest.Mock<any, any>;
+
+jest.mock('@/shared/components/Toast', () => {
+  mockedShowToast = jest.fn();
+
+  return {
+    showToast: mockedShowToast,
+  };
+});
+
+describe('handleErrorMessage Util', () => {
+  it('should show toast error message', () => {
+    handleErrorMessage('error message');
+
+    expect(mockedShowToast).toHaveBeenCalledTimes(1);
+  });
+});
--- src/shared/utils/errorHandler.ts
+++ src/shared/utils/errorHandler.ts
@@ -1,43 +1,5 @@
-//import {Toast} from 'native-base';
-
 import {showToast} from '@/shared/components/Toast';
 
-const getErrorMessage = (code: string) => {
-  switch (code) {
-    case 'auth/user-not-found':
-      return 'Usuário não encontrado';
-    case 'auth/email-already-exists':
-      return 'O email já existe';
-    case 'auth/email-already-in-use':
-      return 'O email já está sendo utilizado';
-    case 'auth/invalid-email':
-      return 'email inválido';
-    case 'auth/invalid-password':
-      return 'Senha inválida';
-    case 'auth/wrong-password':
-      return 'Senha Incorreta';
-    case 'auth/too-many-requests':
-      return 'Muitas tentativas, tente novamente mais tarde';
-    case 'storage/object-not-found':
-      return 'Recurson não encontrado';
-    case 'storage/unauthenticated':
-      return 'usuário não autenticado';
-    case 'storage/retry-limit-exceeded':
-      return 'A requisição demorou tempo demais, tente novamente';
-    case 'storage/invalid-url':
-      return 'Recurson não encontrado';
-    case 'auth/weak-password':
-      return 'A senha deve conter pelo menos 6 caracteres';
-    default:
-      return '';
-  }
-};
-
-/**
- * handleErrorMessage
- * * Verifica qual mensagem de erro foi retornada numa requisição
- * @param {Object} error Objeto de retorno de uma requisição
- */
 export function handleErrorMessage(error: any) {
   console.log('error', error);
   console.log('Message', error.message);
@@ -46,20 +8,7 @@ export function handleErrorMessage(error: any) {
   console.log('namespace', error.namespace);
   console.log('stack', error.stack);
 
-  let message = getErrorMessage(error.code);
-
-  if (!message) {
-    let errorMessage = error.message.replace(/[[\]']+/g, '');
-    errorMessage = errorMessage.split(' ')[1];
-
-    if (errorMessage) {
-      message = getErrorMessage(errorMessage);
-    }
-  }
-
-  if (!message) {
-    message = 'Ocorreu um erro inesperado';
-  }
+  const message = 'Ocorreu um erro inesperado';
 
   showToast({
     text: message,
